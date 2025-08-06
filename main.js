817490f1b9f0a66391344247652d8b060bebf6f0
Mariana Zakseski <140620547+mariszakseski@users.noreply.github.com>
2025-08-06 07:48:32 -0300

    java

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const numeroSenha = document.querySelector('.parametro-senha__texto');
 let tamanhoSenha = 12;
 numeroSenha.textContent = tamanhoSenha;
 
-const botoes = document.querySelectorAll('parametro-senha_botao');
+const botoes = document.querySelectorAll('.parametro-senha_botao');
 
 botoes[0].onclick = diminuiTamanho;
 botoes[1].onclick = aumentaTamanho;
